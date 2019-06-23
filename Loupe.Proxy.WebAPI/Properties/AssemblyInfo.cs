83c039f9ab7f588d9ba34cc5cced1f0c35d1d4c8
Kendall Miller <kendall@gibraltarsoftware.com>
2019-06-23 19:53:43 +0100

    Added NuSpec

--- Loupe.Proxy.WebAPI/Properties/AssemblyInfo.cs
+++ Loupe.Proxy.WebAPI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Loupe.Proxy.WebAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Loupe Proxy for WebAPI")]
+[assembly: AssemblyDescription("WebAPI extension that proxies sesion data from Loupe Agents to a Loupe Server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Loupe.Proxy.WebAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Gibraltar Software, Inc.")]
+[assembly: AssemblyProduct("Loupe")]
+[assembly: AssemblyCopyright("Copyright © 2019 - 2019 Gibraltar Software, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,18 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("840776e8-25d3-4d9a-ae4b-071be0df9202")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.7.0.0")]
+[assembly: AssemblyFileVersion("4.7.0.0")]
